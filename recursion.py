a1d9b3af97f1e5e251fb85e56ab3869e0504fa93
Markéta Rybářová <rybarova.marketa@gmail.com>
2016-11-19 11:12:58 +0100

    rekurze

--- recursion.py
+++ recursion.py
@@ -8,11 +8,26 @@ print(factorial(5))
 
 
 def fibonacci(num):
+    number = 0
     if num == 0:
-        return 0
+        number = 0
     elif num == 1:
-        return 1
+        number = 1
     else:
-        return (fibonacci(num - 2) + fibonacci(num - 1))
+        number = int((fibonacci(num - 2) + fibonacci(num - 1)))
+    print(str(number))
+    return number
 
-print(fibonacci(4))
+fibonacci(4)
+
+
+def GCD(num1, num2):
+    while num2 != 0:
+        r = num1 % num2
+        num1 = num2
+        num2 = r
+
+    result = num1
+    print(result)
+
+GCD(15, 20)
